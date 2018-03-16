986aa8fd6214d2cf156d607efb5715db3badf095
Roulet Etienne <ROETI@coop.ch>
2018-03-16 13:31:47 +0100

    jhelllo

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -23,11 +23,11 @@
                       </div>
                         
                              <div  class="card-columns">
-                                    <div v-for="img  in cardimage" :key="img.url" class="card" id="cardwithpic">
+                                    <div   v-for="img  in cardimage" :key="img.url" class="card" id="cardwithpic">
                                       <img  v-bind:src="img"  class="card-img-top"  alt="Card image cap">
                                       <div class="card-body">
                                         <h5 class="card-title">{{ cardtitle }}</h5>
-                                        <p class="card-text">{{ cardtext }}</p>
+                                        <p  class="card-text">{{ cardtext }}</p>
                                         <a href="#" class="btn btn-light" id="button_PersonData">Person data</a><a href="" class="btn btn-light" id="button_AnpassungPerson">Anpassung Person</a><a href="" class="btn btn-light" id="button_AnpassungDB">Anpassung DB</a>
                                         <p></p>
                                         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
@@ -45,8 +45,8 @@
   data() {
     return {
       cardtitle: "Hello",
-      cardtext: "This is a demo text",
-      cardimage: ['http://blog.usabilla.com/wp-content/uploads/flat_design3.jpg', 'https://cdn.pixabay.com/photo/2018/02/24/18/00/row-pension-3178766_960_720.jpg','http://blog.usabilla.com/wp-content/uploads/flat_design3.jpg', 'https://cdn.pixabay.com/photo/2018/02/24/18/00/row-pension-3178766_960_720.jpg','http://blog.usabilla.com/wp-content/uploads/flat_design3.jpg', 'https://cdn.pixabay.com/photo/2018/02/24/18/00/row-pension-3178766_960_720.jpg','http://blog.usabilla.com/wp-content/uploads/flat_design3.jpg', 'https://cdn.pixabay.com/photo/2018/02/24/18/00/row-pension-3178766_960_720.jpg','http://blog.usabilla.com/wp-content/uploads/flat_design3.jpg', 'https://cdn.pixabay.com/photo/2018/02/24/18/00/row-pension-3178766_960_720.jpg','http://blog.usabilla.com/wp-content/uploads/flat_design3.jpg', 'https://cdn.pixabay.com/photo/2018/02/24/18/00/row-pension-3178766_960_720.jpg' ]
+      cardtext: ['1','2','3'],
+      cardimage: ['http://blog.usabilla.com/wp-content/uploads/flat_design3.jpg', 'https://cdn.pixabay.com/photo/2018/02/24/18/00/row-pension-3178766_960_720.jpg', 'http://blog.usabilla.com/wp-content/uploads/flat_design3.jpg']
 
     }
   },
